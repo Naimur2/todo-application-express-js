56f51a7b05978c556e4a3a9a9b6249becd5fbf64
naimur2 <naimurrahaman248@gmail.com>
2021-09-26 17:42:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+// initialize the app
+const app = express();
+app.use(express.json());
+
+// detabase connection with mongoose
+mongoose
+    .connect('mongodb://localhost:27017/todos')
+    .then(() => console.log('connection successful'))
+    .catch((err) => console.log(err));
+
+
+// application routs
+app.use('/todo',todoHandler)
+
+
+// default error handler
+const errorHandler = (err, req, res, next) => {
+    if (res.headersSent) {
+        return next(err);
+    }
+    return res.status(500).json({ error: err });
+};
+
+app.listen(3000, () => {
+    console.log('app listining at port 3000');
+});
